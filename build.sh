1fa6117717bdf3e0323f69bd9417289c52cbe1bc
Aniket Patil <aniketp563@gmail.com>
2014-06-06 08:23:35 -0400

    Fix merge

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-TOOLCHAIN="/home/shivam/development/arm-linux-androideabi-4.7/bin/arm-linux-androideabi"
-MODULES_DIR="../modules"
-ZIMAGE="/home/shivam/development/kernel/arch/arm/boot/zImage"
-KERNEL_DIR="/home/shivam/development/kernel"
+TOOLCHAIN="/home/aniketp/android_prebuilt_toolchains/arm-linux-androideabi-4.7/bin/arm-linux-androideabi"
+MODULES_DIR="/home/aniketp/modules"
+ZIMAGE="/home/aniketp/phantom_taoshan/arch/arm/boot/zImage"
+KERNEL_DIR="/home/aniketp/phantom_taoshan"
 if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
 then
 rm $ZIMAGE
 rm $MODULES_DIR/*
 fi
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- sa77-kk_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- sa77_defconfig
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
 if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
 then
